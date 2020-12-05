4eb927b1c0517a5091815ef0489d2c7e6993cb56
Phil McMinn <p.mcminn@sheffield.ac.uk>
2020-12-05 17:17:47 +0000

    Adding practicals directory

---
+++ practicals/src/test/java/uk/ac/shef/com3529/TestWeek1.java
@@ -0,0 +1,11 @@
+package uk.ac.shef.com3529.practicals;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestWeek1 {
+
+    // your code goes here
+
+}
