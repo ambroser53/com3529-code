115a8bb60d551aee025dc05532cad033df8bf987
Phil McMinn <phil@mcminn.io>
2021-02-11 20:06:55 +0000

    practicals

--- practicals/src/main/java/uk/ac/shef/com3529/practicals/Week1.java
+++ practicals/src/main/java/uk/ac/shef/com3529/practicals/Week1.java
@@ -5,7 +5,7 @@ public class Week1 {
     /*
      *  If x == null throw a NullPointerException, else return
      *  the index of the last element in x that equals y.
-     *  If no such element exists, return -1
+     *  If no such element exists, return -1.
      */
     public static int findLast(int[] x, int y) {
         for (int i = x.length - 1; i > 0; i--) {
@@ -18,7 +18,7 @@ public class Week1 {
 
     /*
      *  If x == null throw a NullPointerException, else return
-     *  the number of positive elements in x
+     *  the number of positive elements in x.
      */
     public static int countPositive(int[] x) {
         int count = 0;
@@ -32,7 +32,8 @@ public class Week1 {
 
     /*
      *  If x == null throw a NullPointerException, else return
-     *  the number of positive elements in x
+     *  the index of the LAST 0 in x.
+     *  Return -1 if 0 does not occur in x.
      */
     public static int lastZero(int[] x) {
         for (int i = 0; i < x.length; i++) {
@@ -46,7 +47,7 @@ public class Week1 {
     /*
      *  If x == null throw a NullPointerException, else return
      *  the number of elements in x that are either odd or
-     *  positive (or both)
+     *  positive (or both).
      */
     public static int oddOrPos(int[] x) {
         int count = 0;
